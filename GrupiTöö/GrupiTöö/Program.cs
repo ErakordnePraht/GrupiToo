0fc957666b91a5089a3fab6bce61a63b0781ef47
ForTakEr <32396272+ForTakEr@users.noreply.github.com>
2017-11-23 09:20:55 +0200

    It begins

---
+++ GrupiTöö/GrupiTöö/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrupiTöö
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
