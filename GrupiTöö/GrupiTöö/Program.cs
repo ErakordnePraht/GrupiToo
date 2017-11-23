47dd9e796f1762a179d9246f3052bb7f0f2ae743
ForTakEr <32396272+ForTakEr@users.noreply.github.com>
2017-11-23 09:52:40 +0200

    Mingi osa valmis

--- GrupiTöö/GrupiTöö/Program.cs
+++ GrupiTöö/GrupiTöö/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace GrupiTöö
 {
@@ -10,6 +11,34 @@ namespace GrupiTöö
     {
         static void Main(string[] args)
         {
+            StreamReader sr = new StreamReader("kttekst.txt");
+
+            int sPikkus = 0;
+            int luger = 0;
+            string delim = " ,.";
+            string[] fields = null;
+            string line = null;
+
+            while (!sr.EndOfStream)
+            {
+                line = sr.ReadLine();
+                line.Trim();
+                fields = line.Split(delim.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                luger += fields.Length;
+
+                foreach (char letter in luger.ToString())
+                {
+                    sPikkus++;
+                }
+            }
+
+
+            sr.Close();
+            
+            Console.WriteLine("Sõnade arv: {0}", luger);
+            Console.WriteLine("Väiksemad kui 5: {0}", sPikkus);
+
+            Console.ReadLine();
         }
     }
 }
