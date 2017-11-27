470f2402d920ea9e6ffd5048a054b036bbed95c8
Kert Moorbach <33248196+KertM@users.noreply.github.com>
2017-11-27 23:21:59 +0200

    lõpuks ometi
    10010110 101010 10101010

--- GrupiTöö/GrupiTöö/Esimene.cs
+++ GrupiTöö/GrupiTöö/Esimene.cs
@@ -8,5 +8,22 @@ namespace GrupiTöö
 {
     class Esimene
     {
+        Console.WriteLine("Esimene arv: ");
+            int arv1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Teine arv : ");
+            int arv2 = int.Parse(Console.ReadLine());
+
+
+        IEnumerable<int> numbers = Enumerable.Range(arv1, arv2);
+
+            foreach (int number in numbers)
+            {
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+            Console.Read();       
     }
 }
